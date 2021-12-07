53cdfdf80fa9ed28eb8bb1b13b1d9c22e5c91332
hcldivanshu <divanshu.mehta@gmail.com>
2021-12-06 19:18:43 -0700

    branch13-14

--- ppmt-react-client/src/App.js
+++ ppmt-react-client/src/App.js
@@ -1,29 +1,18 @@
 import React, { Component } from "react";
-
-
 import "./App.css";
-
-import Content from "./Components/Content";
-import Content2 from "./Components/Content2";
-import Content3 from "./Components/Content3";
-import Content4 from "./Components/Content4";
-import Content5 from "./Components/Content5";
-import Dashboard from "./Components/Dashboard";
-import Footer from "./Components/Footer";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App-header">
-        
-        <Dashboard/>
-        
-
-        
+      <div className="App">
+        <Header />
+        <Dashboard />
       </div>
     );
   }
 }
-
 
 export default App;
